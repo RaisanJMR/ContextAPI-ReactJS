f1739f7848fe9ef00a297e694cd3a1a5f0d6ba9e
RaisanJMR <raisanjmr@protonmail.com>
2021-07-29 08:18:42 +0530

    finished hooks setup

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import NewSongForm from './components2.js/NewSongForm';
+import SongList from './components2.js/songList';
+
+function App2() {
+  return (
+    <div>
+      <SongList />
+      {/* <NewSongForm /> */}
+    </div>
+  );
+}
+
+export default App2;
